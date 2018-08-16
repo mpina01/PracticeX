7c55dd9bb8c5e29eb9507d36dc1c746b432c68b7
Miriam Pina <mpina@drwholdings.com>
2018-08-16 16:12:12 -0500

    login and home

--- HTML/dynamic/microblog/app/routes.py
+++ HTML/dynamic/microblog/app/routes.py
@@ -1,10 +1,31 @@
-from flask import render_template
+
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
-# ...
 
-@app.route('/login')
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Kou'}
+    posts = [
+        {
+            'author': {'username': 'Futaba'},
+            'body': 'Beautiful day in Tokyo!'
+        },
+        {
+            'author': {'username': 'Haru'},
+            'body': 'Mulan is one of my favorite movies'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',  title='Sign In', form=form)
